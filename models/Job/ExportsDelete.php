314f943440d8178afb9bb816c762dcaf3da7b06f
Jim Safley <jimsafley@gmail.com>
2026-02-12 10:29:55 -0500

    Delete stored ZIP when deleting export

--- models/Job/ExportsDelete.php
+++ models/Job/ExportsDelete.php
@@ -6,7 +6,14 @@ class Job_ExportsDelete extends Job_AbstractExports
      */
     public function perform()
     {
+        // Delete working directory artifacts
         $this->deleteExportDirectory();
         $this->deleteExportZip();
+
+        // Delete the export ZIP file from Omeka storage.
+        $storage = Zend_Registry::get('storage');
+        $storage->delete(
+            sprintf('exports/%s.zip', $this->getExportName())
+        );
     }
 }
